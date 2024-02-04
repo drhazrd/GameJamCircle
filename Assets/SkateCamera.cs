430966abfd764ed167db2c49ed4339746a6fef18
Nick Hatcher <nick@colossalrobot.com>
2024-02-03 19:19:20 -0500

    JetSetMonkey

---
+++ Assets/SkateCamera.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Camera))]
+
+public class SkateCamera : MonoBehaviour
+{
+	
+	private Transform target;
+	private float distanceBehindTarget = 3f;
+	private float height = 0.5f;
+	private float distanceToLookAtInFrontOfTarget = 3f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = target.position - (target.forward * distanceBehindTarget);
+		transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
+		transform.LookAt(target.position + (target.forward * distanceToLookAtInFrontOfTarget));
+	}
+}
---
+++ Assets/SkateMotor.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+
+public class SkateMotor : MonoBehaviour {
+	
+	[SerializeField] private float acceleration = 0.3f;
+	private float currentSpeed = 14f;
+	private float maxSpeed = 20f;
+	private float rotationOnTurnInDeg = 75f;
+	private float xAxisMovementReduction = 1f;//0.18f;
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+		
+		//Make sure the following is true:
+		//inputX + inputY <= 1
+		//Note: some irrelevant calculations to make more readable, optimise later
+		if (inputX + inputY > 1) {
+			float sum = inputX + inputY;
+			float ratio = sum/1;
+			inputX /= ratio;
+			inputY /= ratio;
+		}
+		
+		//Movement
+		float xAxisMovement = inputX * xAxisMovementReduction;
+		float zAxisMovement = inputY * currentSpeed;
+		Vector3 moveDirection;
+		
+		//If only moving left or right we want to emphasise that the character is
+		//skating in a circle so the movement on the x axis is maximised
+		//Otherwise when moving forward and turning, the x axis movement is minimised
+		//if (inputY == 0)
+		//	moveDirection = new Vector3(inputX * currentSpeed, 0, 0) * Time.deltaTime;
+		//else 
+			moveDirection = new Vector3(xAxisMovement, 0, zAxisMovement) * Time.deltaTime;
+		GetComponent<CharacterController>().Move(transform.TransformDirection(moveDirection));
+		
+		//Rotation
+		float yAxisRotation = inputX * rotationOnTurnInDeg;
+		transform.Rotate(new Vector3(0, yAxisRotation, 0) * Time.deltaTime);
+	}
+}

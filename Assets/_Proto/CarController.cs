17c7987d36ef3cb96a6c1ad4136bdfc1eed14be6
Nick Hatcher <nick@colossalrobot.com>
2024-04-20 09:22:46 -0400

    Car game

--- Assets/_Proto/CarController.cs
+++ Assets/_Proto/CarController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
@@ -18,20 +19,58 @@ public class CarController : MonoBehaviour
     public float maxEmission = 25f;
     float emissionRate;
 
+    [Header("Inputs")]
+    public InputActionAsset inputs;
+    InputActionMap carinputmap;
+    InputAction accelerate;
+    InputAction brake;
+    InputAction turn;
+
+
+    void Awake(){
+        carinputmap = inputs.FindActionMap("CarControls");
+        accelerate = inputs.FindAction("Accelerate");
+        brake = inputs.FindAction("Reverse");
+        turn = inputs.FindAction("Turn");
+
+        accelerate.performed += GetAccelerate;
+        accelerate.canceled += GetAccelerate;
+        turn.performed += GetTurn;
+        turn.canceled += GetTurn;
+    }
     void Start(){
         theRB.transform.parent = null;
+    }
+
+    void OnEnable(){
+        accelerate.Enable();
+        turn.Enable();
+    }
+    void OnDisable(){
+        accelerate.Disable();
+        turn.Disable();
+    }
+
+    void GetAccelerate(InputAction.CallbackContext context){
+        speedInput = context.ReadValue<float>() * forwardAccel * 10f;
+        Debug.Log(speedInput);
+    }
+    void GetReverse(InputAction.CallbackContext context){
+        speedInput = context.ReadValue<float>() * reverseAccel;
+    }
+    void GetTurn(InputAction.CallbackContext context){
+        turnInput = context.ReadValue<float>() * turnStrength;
     }
     
     void Update()
     {
-        speedInput = 0f;
-        if(Input.GetAxis("Vertical") > 0){
+        /*if(Input.GetAxis("Vertical") > 0){
             speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
         } else if(Input.GetAxis("Vertical") < 0){
             speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
         }
 
-        turnInput = Input.GetAxis("Horizontal");
+        turnInput = Input.GetAxis("Horizontal");*/
 
         if(speedInput != 0  && grounded){
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3 (0f, turnInput * turnStrength * Time.deltaTime, 0f));

bc393f4dbef3a3da48a0d76de05fdcdff324f6aa
Nick Hatcher <nick@colossalrobot.com>
2024-09-27 13:23:09 -0400

    RTS Building VBalance

--- Assets/_Scripts/Building.cs
+++ Assets/_Scripts/Building.cs
@@ -12,9 +12,12 @@ public class Building : MonoBehaviour
     public MobUnit mobSpawn;
     float timeBetweenSpawn = 2.5f;
     public bool autoSpawn;
+    public bool canSpawn;
     bool spawnReady;
     Transform resource;
     Transform mainBase;
+    int currentSpawn;
+    public int maxSpawn = 5;
 
     void Start(){
         if(spawnArea == null){
@@ -26,17 +29,20 @@ public class Building : MonoBehaviour
     }
 
     public void SpawnMob(){
+        autoSpawn = currentSpawn < maxSpawn;
         if(mobSpawn != null && !spawnReady){
             MobUnit newUnit = Instantiate(mobSpawn, spawnArea.position, spawnArea.rotation) as MobUnit;
             newUnit.UpdateTargets(transform, resource);
             spawnReady = true;
         }
+
     }
 
     IEnumerator MobSpawnProcess(float time){
-        while(autoSpawn){
+        while(autoSpawn && canSpawn){
             yield return new WaitForSeconds(time);
             SpawnMob();
+            currentSpawn++;
             spawnReady = false;
         }
     }
